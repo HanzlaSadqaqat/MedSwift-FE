474f3efa0e459b47f7966ff51a433709044fba9a
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2023-08-07 09:40:00 -0700

    Chore: Add to card Functionality

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,17 +4,29 @@ import { useState, useEffect, useContext } from "react";
 // import { Card } from "../components/homepage/Card";
 import axios, { AxiosError } from "axios";
 import { AppContextData } from "../context/AppContext";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 interface sendDataProp {
   imageUrl: string[];
   name: string;
   price: number;
+  _id: string;
 }
 export const HomePage: React.FC = () => {
   const [result, setResult] = useState([]);
   const { email } = useContext(AppContextData);
+  const [items, setItems] = useState(() => {
+    const storeItems = localStorage.getItem("items");
+    return storeItems ? JSON.parse(storeItems) : [];
+  });
+
   useEffect(() => {
     getCardData();
   }, []);
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   const getCardData = async () => {
     try {
@@ -26,6 +38,25 @@ export const HomePage: React.FC = () => {
     }
   };
 
+  const addToCart = (
+    id: string,
+    name: string,
+    price: number,
+    imageUrl: string[]
+  ) => {
+    const newItem = {
+      name: name,
+      price: price,
+      imageUrl: imageUrl,
+      id: id,
+    };
+    const updateItems = [...items, newItem];
+    setItems(updateItems);
+    // localStorage.setItem("items", JSON.stringify(updateItems));
+
+    console.log(updateItems);
+  };
+
   return (
     <div>
       <Navbar email={email}>
@@ -35,9 +66,32 @@ export const HomePage: React.FC = () => {
         <div className="grid grid-cols-4">
           {result.map((res: sendDataProp) => {
             return (
-              <Card imageUrl={res.imageUrl} name={res.name} price={res.price}>
-                <div></div>
-              </Card>
+              <Link
+                to="/product/details"
+                className="border  p-4 flex flex-col gap-2 shadow-sm rounded hover:shadow-xl m-2"
+              >
+                <Card
+                  imageUrl={res.imageUrl}
+                  name={res.name}
+                  price={res.price}
+                  id={res._id}
+                >
+                  <div>
+                    <div className="flex gap-3 justify-center">
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          addToCart(res._id, res.name, res.price, res.imageUrl);
+                        }}
+                        className="px-4 w-5/6 flex justify-center py-2 bg-blue-400 rounded hover:bg-blue-500 transition duration-200 text-white font-bold gap-2 items-center"
+                      >
+                        <FontAwesomeIcon icon={faCartShopping} />
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </Card>
+              </Link>
             );
           })}
         </div>
